7dfcedf0ef0f853be530e1d7ef20f2ebfb146704
Nabil Almidani <nabilmidani@nabils-mbp.home>
2017-01-27 08:20:33 -0500

    Initial Commit

---
+++ Xcod8GitExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcod8GitExample
+//
+//  Created by Nabil Almidani on 1/27/17.
+//  Copyright © 2017 Nabil Almidani. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcod8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcod8GitExample
+//
+//  Created by Nabil Almidani on 1/27/17.
+//  Copyright © 2017 Nabil Almidani. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
